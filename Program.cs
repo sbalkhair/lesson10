de2df8d8f141cf87a89b5b8add3c6a594142353b
saeed <s.balkhair@hotmail.com>
2020-10-29 09:13:46 +0300

    Initial check-in of module lesson10

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace lesson10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            #region Quistion2
+            //Console.WriteLine("Enter numbers split it by - widthout dublcate number");
+            //var input = Console.ReadLine();
+
+            //var inputArray = input.Split('-');
+
+            //List<string> numbers = new List<string>();
+
+            //string message = "";
+
+            //foreach (var item in inputArray)
+            //{
+            //    if (numbers.Contains(item))
+            //    {
+            //        message = "yes";
+            //        break;
+            //    }
+
+            //    numbers.Add(item);
+
+            //}
+
+
+
+            //if (message == "yes")
+            //{
+            //    Console.WriteLine("dublcate number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not dublcate number");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region Quistion4
+            //Console.WriteLine("Enter any text");
+            //var input = Console.ReadLine();
+
+            //var arrayOfString = input.ToLower().Split(' ');
+
+
+            //string outputString = "";
+            //foreach (var item in arrayOfString)
+            //{
+            //    // roya altamimi
+            //    var firstLetter = item.Substring(0, 1).ToUpper();
+            //    var remainLetter = item.Substring(1);
+            //    outputString += $"{firstLetter}{remainLetter}";
+
+            //}
+
+            //Console.WriteLine(outputString);
+            //Console.ReadKey();
+            #endregion
+
+            #region Quistion5
+            //Console.WriteLine("Enter any text");
+            //var input = Console.ReadLine();
+
+            //var letters = input.ToCharArray();
+
+            //int count = 0;
+
+            //foreach (var letter in letters)
+            //{
+            //    if (letter == 'a' || letter == 'e' || letter == 'o' || letter == 'u' || letter == 'i')
+            //    {
+            //        count++;
+            //    }
+            //}
+
+
+            //Console.WriteLine($"count of :{count}");
+            //Console.ReadKey();
+            #endregion
+
+        }
+    }
+}
